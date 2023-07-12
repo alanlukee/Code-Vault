b74b274d6484006dab6617a5677d03e329c8812d
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2023-07-12 22:46:28 +0530

    Time: 1219 ms (16.38%), Space: 13.3 MB (68.46%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x == 0:
+            return 0
+        i = 1
+        while i * i <= x:
+            i += 1
+        
+        return i - 1
