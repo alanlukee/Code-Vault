e57f5322b874972f721886d787913f861eddde96
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2023-07-13 13:13:23 +0530

    Delete 0069-sqrtx directory

--- 0069-sqrtx/0069-sqrtx.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def mySqrt(self, x):
-        if x == 0:
-            return 0
-        i = 1
-        while i * i <= x:
-            i += 1
-        
-        return i - 1
