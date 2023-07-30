6874529f7d67208855b7b85bc00d2d48460214ec
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2023-07-30 14:36:45 +0530

    Time: 343 ms (94.07%), Space: 13.4 MB (90.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -10,24 +10,69 @@
 #                         maxi = len(s[i:j][::-1])
 #                         longest = s[i:j]
 #         return longest
+
+
+# class Solution:
+#     def longestPalindrome(self, s):
+#         def check(i, j):
+#             left = i
+#             right = j - 1
+            
+#             while left < right:
+#                 if s[left] != s[right]:
+#                     return False
+                
+#                 left = left + 1
+#                 right = right - 1
+            
+#             return True
+        
+#         for length in range(len(s), 0, -1):
+#             for start in range(len(s) - length + 1):
+#                 if check(start, start + length):
+#                     return s[start:start + length]
+
+#         return ""
+
 class Solution:
     def longestPalindrome(self, s):
-        def check(i, j):
-            left = i
-            right = j - 1
-            
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                
-                left = left + 1
-                right = right - 1
-            
-            return True
-        
-        for length in range(len(s), 0, -1):
-            for start in range(len(s) - length + 1):
-                if check(start, start + length):
-                    return s[start:start + length]
+        def expand(l,r):
+            while(l>=0 and r<len(s) and s[l] == s[r]):
+                l = l-1
+                r = r+1
+            return s[l+1:r]
 
-        return ""
+        result = ""
+        for i in range(len(s)):
+            subodd = expand(i,i)
+            if (len(subodd)>len(result)):
+                result = subodd
+            subeven = expand(i,i+1)
+            if (len(subeven)>len(result)):
+                result = subeven
+        return result
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
